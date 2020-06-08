b75ba4a92fb5fbf852e3e42782e1a1dcd2a0eced
Léo Lam <leo@leolam.fr>
2020-06-08 23:46:29 +0200

    Endian: Silence warnings

--- src/seadEndian.cpp
+++ src/seadEndian.cpp
@@ -22,7 +22,7 @@ u16 Null16(u16 val)
 
 u32 Swap32(u32 val)
 {
-    return val << 24 | (val & 0xFF00) << 8 | val >> 24 | val >> 8 & 0xFF00;
+    return val << 24 | (val & 0xFF00) << 8 | val >> 24 | (val >> 8 & 0xFF00);
 }
 
 u32 Null32(u32 val)
@@ -35,7 +35,7 @@ u64 Swap64(u64 val)
     // Couldn't make an implementation that matches the original assembly
     // But this should be much more efficient
     return val << 56 | (val & 0xFF00) << 40 | (val & 0xFF0000) << 24 | (val & 0xFF000000) << 8 |
-           val >> 56 | val >> 40 & 0xFF00 | val >> 24 & 0xFF0000 | val >> 8 & 0xFF000000;
+           val >> 56 | (val >> 40 & 0xFF00) | (val >> 24 & 0xFF0000) | (val >> 8 & 0xFF000000);
 }
 
 u64 Null64(u64 val)
