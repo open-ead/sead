f8ab7a9b9aa4f23d1d23d624366fcba81ce98855
Léo Lam <leo@leolam.fr>
2020-06-08 23:37:28 +0200

    System: Add header

---
+++ include/sead/seadSystem.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstdarg>
+#include <sead/types.h>
+
+#ifdef SEAD_DEBUG
+#define SEAD_ASSERT(condition, message, ...)                                                       \
+    do                                                                                             \
+    {                                                                                              \
+        if (!(condition))                                                                          \
+            sead::system::HaltWithDetail(__FILE__, __LINE__, message, __VA_ARGS__);                \
+    } while (0)
+#else
+#define SEAD_ASSERT(condition, message, ...)
+#endif
+
+namespace sead
+{
+class Heap;
+
+namespace system
+{
+void Halt();
+void HaltWithDetail(const char* file, int line, const char* msg, ...);
+void HaltWithDetailNoFormat(const char* file, int line, const char* msg);
+void DebugBreak();
+
+void Print(const char* format, ...);
+void PrintV(const char* format, std::va_list);
+void PrintString(const char* format, s32);
+
+void Warning(const char* file, int line, const char* msg, ...);
+void SetWarningEnable(bool enable);
+}  // namespace system
+
+}  // namespace sead
