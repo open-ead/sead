39a636393456658de91f30ffe1400bee23d248a1
Léo Lam <leo@leolam.fr>
2020-06-12 22:45:35 +0200

    cmake: Fix build

--- include/math/seadMathCalcCommon.h
+++ include/math/seadMathCalcCommon.h
@@ -8,7 +8,7 @@ template <typename T>
 class MathCalcCommon
 {
 public:
-    static s32 roundUpPow2(T x, s32 y) { return x + y - 1 & (u32)-y; }
+    static s32 roundUpPow2(T x, s32 y);
 
     static s32 roundDownPow2(T x, s32 y);
     static T gcd(T x, T y);
