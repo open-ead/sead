a8c796696d5de0fb8fbf40990b92b74ff55f9485
Léo Lam <leo@leolam.fr>
2020-06-17 00:11:00 +0200

    time/TickTime: Implement (with setNow only implemented for Switch)

---
+++ include/time/seadTickTime.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#ifdef SWITCH
+#include <nn/os.h>
+#endif
+
+#include <basis/seadTypes.h>
+#include <time/seadTickSpan.h>
+
+namespace sead
+{
+/// A TickTime represents an instant in time.
+class TickTime
+{
+public:
+    TickTime() { setNow(); }
+
+    u64 toTicks() const { return mTick; }
+
+#ifdef SWITCH
+    void setNow() { mTick = nn::os::GetSystemTick(); }
+#else
+    void setNow();
+#endif
+
+    TickSpan diff(const TickTime& other) const { return s64(mTick) - s64(other.mTick); }
+    TickSpan diffToNow() const { return diff(TickTime()); }
+
+    TickTime& operator+=(const TickSpan& span)
+    {
+        mTick += span.toTicks();
+        return *this;
+    }
+
+    TickTime& operator-=(const TickSpan& span)
+    {
+        mTick -= span.toTicks();
+        return *this;
+    }
+
+private:
+    u64 mTick;
+};
+}  // namespace sead
