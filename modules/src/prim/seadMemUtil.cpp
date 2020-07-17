b8619d5ebabf27265c64e59ca6261d60e319711d
Léo Lam <leo@leolam.fr>
2020-07-17 19:12:14 +0200

    prim/MemUtil: Fix invalid format strings and UB

--- modules/src/prim/seadMemUtil.cpp
+++ modules/src/prim/seadMemUtil.cpp
@@ -6,9 +6,9 @@ namespace sead
 {
 void* MemUtil::copyAlign32(void* dst, const void* src, size_t size)
 {
-    SEAD_ASSERT(size % 32 == 0, "size % 32 == 0  size:%llu", size);
+    SEAD_ASSERT(size % 32 == 0, "size %% 32 == 0  size:%zu", size);
     SEAD_ASSERT(PtrUtil::isAligned(dst, 32) && PtrUtil::isAligned(src, 32),
-                "pointer must be 32-byte aligned. src:%#0p -> dst:%#0p", src, dst);
+                "pointer must be 32-byte aligned. src:%p -> dst:%p", src, dst);
     return copy(dst, src, size);
 }
 
