715ebf767ae4ea912a968e396fafcbc55171e1ae
Léo Lam <leo@leolam.fr>
2020-07-17 15:22:53 +0200

    basis/RawPrint: Check format strings

--- include/basis/seadRawPrint.h
+++ include/basis/seadRawPrint.h
@@ -22,24 +22,50 @@
         sead::system::Warning(__FILE__, __LINE__, message, ##__VA_ARGS__);                         \
     while (0)
 #else
-#define SEAD_ASSERT(condition, message, ...)
-#define SEAD_ASSERT_NOFMT(condition)
-#define SEAD_WARN(message, ...)
+#define SEAD_ASSERT(condition, message, ...)                                                       \
+    do                                                                                             \
+    {                                                                                              \
+        if (!(condition))                                                                          \
+            sead::system::detail::CheckFormat(message, ##__VA_ARGS__);                             \
+    } while (0)
+#define SEAD_ASSERT_NOFMT(condition) static_cast<void>(condition)
+#define SEAD_WARN(message, ...) sead::system::detail::CheckFormat(message, ##__VA_ARGS__)
 #endif
 
 namespace sead
 {
 namespace system
 {
+namespace detail
+{
+// Dummy function whose only purpose is to trigger a format string check.
+#ifdef __GNUC__
+[[maybe_unused]] [[gnu::format(printf, 1, 2)]]
+#endif
+inline void
+CheckFormat(const char*, ...)
+{
+}
+}  // namespace detail
+
 void Halt();
+#ifdef __GNUC__
+[[gnu::format(printf, 3, 4)]]
+#endif
 void HaltWithDetail(const char* file, int line, const char* msg, ...);
 void HaltWithDetailNoFormat(const char* file, int line, const char* msg);
 void DebugBreak();
 
+#ifdef __GNUC__
+[[gnu::format(printf, 1, 2)]]
+#endif
 void Print(const char* format, ...);
 void PrintV(const char* format, std::va_list);
 void PrintString(const char* format, s32);
 
+#ifdef __GNUC__
+[[gnu::format(printf, 3, 4)]]
+#endif
 void Warning(const char* file, int line, const char* msg, ...);
 void SetWarningEnable(bool enable);
 }  // namespace system
