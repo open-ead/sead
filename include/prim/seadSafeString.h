bf61052e5461bb4a3e3219089250b98c76f3a602
Léo Lam <leo@leolam.fr>
2020-07-17 22:30:29 +0200

    prim/SafeString: Declare or implement more SafeString functions
    Also changes some existing functions (comparen, findIndex) for better
    accuracy with recent sead
    
    This commit also adds or changes BufferedSafeStringBase functions that
    are required by SafeStringBase

--- include/prim/seadSafeString.h
+++ include/prim/seadSafeString.h
@@ -1,22 +1,86 @@
-#ifndef SEAD_SAFE_STRING_H_
-#define SEAD_SAFE_STRING_H_
+#pragma once
 
 #ifndef SEAD_SAFE_STRING_HPP_
 #error "Include <sead/seadSafeString.hpp>"
 #endif
 
-#include <stdarg.h>
+#include <cstdarg>
 
 #include <basis/seadTypes.h>
 
 namespace sead
 {
 template <typename T>
+class BufferedSafeStringBase;
+
+template <typename T>
 class SafeStringBase
 {
 public:
+    /// Iterates over every character of a string.
+    /// Note that this is extremely inefficient and leads to quadratic time complexity
+    /// because of the redundant calls to calcLength() in operator*.
+    class iterator
+    {
+    public:
+        explicit iterator(const SafeStringBase* string) : mString(string), mIndex(0) {}
+        iterator(const SafeStringBase* string, s32 index) : mString(string), mIndex(index) {}
+        bool operator==(const iterator& rhs) const
+        {
+            return mString == rhs.mString && mIndex == rhs.mIndex;
+        }
+        bool operator!=(const iterator& rhs) const { return !(rhs == *this); }
+        iterator& operator++() { return mIndex++, *this; }
+        iterator& operator--() { return mIndex--, *this; }
+        const char& operator*() const { return mString->at(mIndex); }
+
+        const SafeStringBase* getString() const { return mString; }
+        s32 getIndex() const { return mIndex; }
+
+    protected:
+        const SafeStringBase* mString;
+        s32 mIndex;
+    };
+
+    /// Iterates over a string as if it were split by one or several delimiter characters.
+    class token_iterator : public iterator
+    {
+    public:
+        token_iterator(const SafeStringBase* string, const SafeStringBase& delimiter)
+            : iterator(string), mDelimiter(delimiter)
+        {
+        }
+
+        token_iterator(const SafeStringBase* string, s32 index, const SafeStringBase& delimiter)
+            : iterator(string, index), mDelimiter(delimiter)
+        {
+        }
+
+        bool operator==(const token_iterator& rhs) const
+        {
+            return static_cast<const iterator&>(*this) == static_cast<const iterator&>(rhs) &&
+                   mDelimiter == rhs.mDelimiter;
+        }
+        bool operator!=(const token_iterator& rhs) const { return !(rhs == *this); }
+
+        token_iterator& operator++();
+        token_iterator& operator--();
+
+        s32 get(BufferedSafeStringBase<T>* out) const;
+        s32 getAndForward(BufferedSafeStringBase<T>* out);
+        s32 cutOffGet(BufferedSafeStringBase<T>* out) const;
+        s32 cutOffGetAndForward(BufferedSafeStringBase<T>* out);
+
+    private:
+        const SafeStringBase mDelimiter;
+    };
+
     SafeStringBase() : mStringTop(cNullString) {}
-    SafeStringBase(const T* str) : mStringTop(str) {}
+    SafeStringBase(const T* str) : mStringTop(str)
+    {
+        SEAD_ASSERT(str != nullptr, "str must not be nullptr.");
+    }
+
     virtual ~SafeStringBase() = default;
 
     virtual SafeStringBase& operator=(const SafeStringBase& other)
@@ -25,35 +89,60 @@ public:
         return *this;
     }
 
-    virtual void assureTerminationImpl_() const;
+    bool operator==(const SafeStringBase& rhs) const { return isEqual(rhs); }
+    bool operator!=(const SafeStringBase& rhs) const { return !(rhs == *this); }
 
-    inline const T* cstr() const
+    iterator begin() const { return iterator(this, 0); }
+    iterator end() const { return iterator(this, calcLength()); }
+
+    token_iterator tokenBegin(const SafeStringBase& delimiter)
+    {
+        return token_iterator(this, delimiter);
+    }
+
+    token_iterator tokenEnd(const SafeStringBase& delimiter)
+    {
+        return token_iterator(this, calcLength(), delimiter);
+    }
+
+    const T* cstr() const
     {
         assureTerminationImpl_();
         return mStringTop;
     }
 
-    inline const SafeStringBase<T> getPart(s32 at) const
-    {
-        s32 len = calcLength();
-        if (at < 0 || at > len)
-            return SafeStringBase<T>::cEmptyString;
-
-        return SafeStringBase<T>(mStringTop + at);
-    }
-
-    inline const T& unsafeAt_(s32 idx) const { return mStringTop[idx]; }
+    inline const T& at(s32 idx) const;
+    inline const T& operator[](s32 idx) const { return at(idx); }
 
     inline s32 calcLength() const;
-    inline bool isEqual(const SafeStringBase<T>& str) const;
-    inline s32 comparen(const SafeStringBase<T>& str, s32 n) const;
-    inline s32 findIndex(const SafeStringBase<T>& str) const;
+
+    inline SafeStringBase<T> getPart(s32 at) const;
+    inline SafeStringBase<T> getPart(const iterator& it) const;
+    inline SafeStringBase<T> getPart(const token_iterator& it) const;
+
+    inline bool include(const T& c) const;
+    inline bool include(const SafeStringBase<T>& str) const;
+
+    bool isEqual(const SafeStringBase<T>& str) const;
+    s32 comparen(const SafeStringBase<T>& str, s32 n) const;
+
+    s32 findIndex(const SafeStringBase<T>& str) const { return findIndex(str, 0); }
+    s32 findIndex(const SafeStringBase<T>& str, s32 start_pos) const;
+    s32 rfindIndex(const SafeStringBase<T>& str) const;
+
+    bool isEmpty() const;
+    bool startsWith(const SafeStringBase<T>& prefix) const;
+    bool endsWith(const SafeStringBase<T>& suffix) const;
 
     static const T cNullChar;
     static const T cNullString[1];
     static const T cLineBreakChar;
     static const SafeStringBase cEmptyString;
     static const s32 cMaximumLength = 0x80000;
+
+protected:
+    virtual void assureTerminationImpl_() const;
+    const T& unsafeAt_(s32 idx) const { return mStringTop[idx]; }
 
     const T* mStringTop;
 };
@@ -105,6 +194,8 @@ public:
 
     inline s32 copy(const SafeStringBase<T>& src, s32 copyLength = -1);
     inline s32 copyAt(s32 at, const SafeStringBase<T>& src, s32 copyLength = -1);
+    inline s32 cutOffCopy(const SafeStringBase<T>& src, s32 copyLength = -1);
+    inline s32 cutOffCopyAt(s32 at, const SafeStringBase<T>& src, s32 copyLength = -1);
     inline s32 trim(s32 trimLength);
 
     s32 mBufferSize;
@@ -189,5 +280,3 @@ public:
 };
 
 }  // namespace sead
-
-#endif  // SEAD_SAFE_STRING_H_
--- include/prim/seadSafeString.hpp
+++ include/prim/seadSafeString.hpp
@@ -8,8 +8,212 @@
 namespace sead
 {
 template <typename T>
+inline typename SafeStringBase<T>::token_iterator& SafeStringBase<T>::token_iterator::operator++()
+{
+    s32 index = this->mIndex;
+    const s32 length = this->mString->calcLength();
+    if (0 <= index && index <= length)
+    {
+        while (true)
+        {
+            SEAD_ASSERT_NOFMT(0 <= index && index <= length);
+            if (this->mString->unsafeAt_(index) == cNullChar)
+                break;
+            if (mDelimiter.include(this->mString->unsafeAt_(index)))
+                break;
+            ++index;
+        }
+
+        this->mIndex = index + 1;
+    }
+    else
+    {
+        SEAD_ASSERT(false, "index(%d) out of range [0, %d].\n", index, length);
+    }
+    return *this;
+}
+
+template <typename T>
+inline typename SafeStringBase<T>::token_iterator& SafeStringBase<T>::token_iterator::operator--()
+{
+    s32 index = this->mIndex;
+    const s32 length = this->mString->calcLength();
+
+    if (index == 0)
+        return *this;
+
+    if (index == 1)
+    {
+        this->mIndex = 0;
+        return *this;
+    }
+
+    if (0 <= index && index <= length + 1)
+    {
+        index -= 2;
+        s32 j;
+        while (true)
+        {
+            j = index;
+            SEAD_ASSERT_NOFMT(0 <= index && index <= length);
+            if (this->mString->unsafeAt_(index) == cNullChar)
+                break;
+            if (mDelimiter.include(this->mString->unsafeAt_(index)))
+                break;
+            --index;
+            if (j == 0)
+            {
+                j = index;
+                break;
+            }
+        }
+
+        this->mIndex = j + 1;
+    }
+    else
+    {
+        SEAD_ASSERT(false, "index(%d) out of range [0, %d].\n", index, length + 1);
+    }
+    return *this;
+}
+
+template <typename T>
+inline s32 SafeStringBase<T>::token_iterator::get(BufferedSafeStringBase<T>* out) const
+{
+    token_iterator it = *this;
+    ++it;
+    const s32 part_length = it.getIndex() - this->getIndex() - 1;
+
+    const SafeStringBase<T> part = this->mString->getPart(*this);
+    return out->copy(part, part_length);
+}
+
+template <typename T>
+inline s32 SafeStringBase<T>::token_iterator::getAndForward(BufferedSafeStringBase<T>* out)
+{
+    // NOT_MATCHING(SEAD_DEBUG): minor regalloc differences.
+
+    s32 index = this->mIndex;
+    const s32 length = this->mString->calcLength();
+    if (index < 0 || index > length)
+    {
+        SEAD_ASSERT(false, "index(%d) out of range [0, %d].\n", index, length);
+        return 0;
+    }
+
+    out->assureTerminationImpl_();
+    T* outc = out->getBuffer();
+    const s32 out_max_length = out->getBufferSize() - 1;
+
+    s32 i = 0;
+    while (true)
+    {
+        SEAD_ASSERT_NOFMT(0 <= index && index <= length);
+        if (out_max_length < i)
+        {
+            SEAD_ASSERT(false, "token str exceeds out buffer length[%d]", out_max_length);
+            return 0;
+        }
+
+        const T& c = this->mString->unsafeAt_(index);
+        if (c == cNullChar || mDelimiter.include(c))
+            break;
+
+        outc[i++] = c;
+        ++index;
+    }
+
+    outc[i] = cNullChar;
+    this->mIndex = index + 1;
+    return i;
+}
+
+template <typename T>
+inline s32 SafeStringBase<T>::token_iterator::cutOffGet(BufferedSafeStringBase<T>* out) const
+{
+    token_iterator it = *this;
+    ++it;
+    const s32 part_length = it.getIndex() - this->getIndex() - 1;
+
+    const SafeStringBase<T> part = this->mString->getPart(this->getIndex());
+    return out->cutOffCopy(part, part_length);
+}
+
+template <typename T>
+inline s32 SafeStringBase<T>::token_iterator::cutOffGetAndForward(BufferedSafeStringBase<T>* out)
+{
+    s32 index = this->mIndex;
+    const s32 length = this->mString->calcLength();
+    if (index < 0 || index > length)
+    {
+        SEAD_ASSERT(false, "index(%d) out of range [0, %d].\n", index, length);
+        return 0;
+    }
+
+    out->assureTerminationImpl_();
+    T* outc = out->getBuffer();
+    const s32 out_max_length = out->getBufferSize() - 1;
+
+    s32 i = 0;
+    while (true)
+    {
+        SEAD_ASSERT_NOFMT(0 <= index && index <= length);
+
+        const T& c = this->mString->unsafeAt_(index);
+        if (c == cNullChar || mDelimiter.include(c))
+            break;
+
+        if (i < out_max_length)
+            outc[i++] = c;
+        ++index;
+    }
+
+    SEAD_ASSERT_NOFMT(i <= out_max_length);
+    outc[i] = cNullChar;
+    this->mIndex = index + 1;
+    return i;
+}
+
+template <typename T>
+inline const T& SafeStringBase<T>::at(s32 idx) const
+{
+    const int length = calcLength();
+    if (idx >= 0 && idx < length)
+        return mStringTop[idx];
+
+    SEAD_ASSERT(false, "index(%d) out of range[0, %d]", idx, length);
+    return cNullChar;
+}
+
+template <typename T>
+inline SafeStringBase<T> SafeStringBase<T>::getPart(s32 at) const
+{
+    s32 len = calcLength();
+    if (at < 0 || at > len)
+    {
+        SEAD_ASSERT(false, "index(%d) out of range[0, %d]", at, len);
+        return SafeStringBase<T>::cEmptyString;
+    }
+
+    return SafeStringBase<T>(mStringTop + at);
+}
+
+template <typename T>
+inline SafeStringBase<T> SafeStringBase<T>::getPart(const SafeStringBase::iterator& it) const
+{
+    return getPart(it.getIndex());
+}
+
+template <typename T>
+inline SafeStringBase<T> SafeStringBase<T>::getPart(const SafeStringBase::token_iterator& it) const
+{
+    return getPart(it.getIndex());
+}
+
+template <typename T>
 inline s32 SafeStringBase<T>::calcLength() const
 {
+    SEAD_ASSERT_NOFMT(mStringTop);
     assureTerminationImpl_();
     s32 length = 0;
 
@@ -22,9 +226,32 @@ inline s32 SafeStringBase<T>::calcLength() const
     }
 
     if (length > cMaximumLength)
+    {
+        SEAD_ASSERT(false, "too long string");
         return 0;
+    }
 
     return length;
+}
+
+template <typename T>
+inline bool SafeStringBase<T>::include(const T& c) const
+{
+    assureTerminationImpl_();
+    for (s32 i = 0; i <= cMaximumLength; ++i)
+    {
+        if (unsafeAt_(i) == cNullChar)
+            break;
+        if (unsafeAt_(i) == c)
+            return true;
+    }
+    return false;
+}
+
+template <typename T>
+inline bool SafeStringBase<T>::include(const SafeStringBase<T>& str) const
+{
+    return findIndex(str) != -1;
 }
 
 template <typename T>
@@ -39,10 +266,11 @@ inline bool SafeStringBase<T>::isEqual(const SafeStringBase<T>& str) const
         if (unsafeAt_(i) != str.unsafeAt_(i))
             return false;
 
-        else if (unsafeAt_(i) == cNullChar)
+        if (unsafeAt_(i) == cNullChar)
             return true;
     }
 
+    SEAD_ASSERT(false, "too long string\n");
     return false;
 }
 
@@ -54,44 +282,157 @@ inline s32 SafeStringBase<T>::comparen(const SafeStringBase<T>& str, s32 n) cons
         return 0;
 
     if (n > cMaximumLength)
-        n = cMaximumLength;
-
-    for (s32 i = 0; i < n; i++)
     {
-        if (unsafeAt_(i) == cNullChar && str.unsafeAt_(i) == cNullChar)
+        SEAD_ASSERT(false, "paramater(%d) out of bounds [0, %d]", n, cMaximumLength);
+        n = cMaximumLength;
+    }
+
+    for (s32 i = 0; i < n; ++i)
+    {
+        if (unsafeAt_(i) < str.unsafeAt_(i))
+            return -1;
+
+        if (unsafeAt_(i) > str.unsafeAt_(i))
+            return 1;
+
+        if (unsafeAt_(i) == cNullChar)
             return 0;
-
-        else if (unsafeAt_(i) == cNullChar)
-            return -1;
-
-        else if (str.unsafeAt_(i) == cNullChar)
-            return 1;
-
-        else if (unsafeAt_(i) < str.unsafeAt_(i))
-            return -1;
-
-        else if (unsafeAt_(i) > str.unsafeAt_(i))
-            return 1;
     }
 
     return 0;
 }
 
 template <typename T>
-inline s32 SafeStringBase<T>::findIndex(const SafeStringBase<T>& str) const
+inline s32 SafeStringBase<T>::findIndex(const SafeStringBase<T>& str, s32 start_pos) const
 {
-    s32 len = calcLength();
-    s32 subStrLen = str.calcLength();
+    const s32 len = calcLength();
 
-    for (s32 i = 0; i <= len - subStrLen; i++)
-        if (SafeStringBase<T>(&mStringTop[i]).comparen(str, subStrLen) == 0)
+    if (start_pos < 0 || start_pos > len)
+    {
+        SEAD_ASSERT(false, "start_pos(%d) out of range[0, %d]", start_pos, len);
+        return -1;
+    }
+
+    const s32 sub_str_len = str.calcLength();
+
+    for (s32 i = start_pos; i <= len - sub_str_len; ++i)
+    {
+        if (SafeStringBase<T>(&mStringTop[i]).comparen(str, sub_str_len) == 0)
             return i;
-
+    }
     return -1;
 }
 
 template <typename T>
+inline s32 SafeStringBase<T>::rfindIndex(const SafeStringBase<T>& str) const
+{
+    const s32 len = calcLength();
+    const s32 sub_str_len = str.calcLength();
+
+    for (s32 i = len - sub_str_len; i >= 0; --i)
+    {
+        if (SafeStringBase<T>(&mStringTop[i]).comparen(str, sub_str_len) == 0)
+            return i;
+    }
+    return -1;
+}
+
+template <typename T>
+inline bool SafeStringBase<T>::isEmpty() const
+{
+    return unsafeAt_(0) == '\0';
+}
+
+template <typename T>
+inline bool SafeStringBase<T>::startsWith(const SafeStringBase<T>& prefix) const
+{
+    return findIndex(prefix) == 0;
+}
+
+template <typename T>
+inline bool SafeStringBase<T>::endsWith(const SafeStringBase<T>& suffix) const
+{
+    const s32 sub_str_len = suffix.calcLength();
+    if (sub_str_len == 0)
+        return true;
+
+    const T* strc = mStringTop;
+    const T* suffixc = suffix.mStringTop;
+
+    const s32 len = calcLength();
+    if (len < sub_str_len)
+        return false;
+
+    for (s32 i = 0; i < sub_str_len; ++i)
+    {
+        if (strc[len - sub_str_len + i] != suffixc[i])
+            return false;
+    }
+    return true;
+}
+
+template <typename T>
 inline s32 BufferedSafeStringBase<T>::copy(const SafeStringBase<T>& src, s32 copyLength)
+{
+    T* dst = getMutableStringTop_();
+    const T* csrc = src.cstr();
+    if (dst == csrc)
+        return 0;
+
+    if (copyLength < 0)
+        copyLength = src.calcLength();
+
+    if (copyLength >= mBufferSize)
+    {
+        SEAD_ASSERT(false, "Buffer overflow. (Buffer Size: %d, Copy Size: %d)", mBufferSize,
+                    copyLength);
+        copyLength = mBufferSize - 1;
+    }
+
+    MemUtil::copy(dst, csrc, copyLength * sizeof(T));
+    dst[copyLength] = SafeStringBase<T>::cNullChar;
+
+    return copyLength;
+}
+
+template <typename T>
+inline s32 BufferedSafeStringBase<T>::copyAt(s32 at, const SafeStringBase<T>& src, s32 copyLength)
+{
+    T* dst = getMutableStringTop_();
+    s32 len = this->calcLength();
+
+    if (at < 0)
+    {
+        at = len + at + 1;
+        if (at < 0)
+        {
+            SEAD_ASSERT(false, "at(%d) out of range[0, %d]", at, len);
+            at = 0;
+        }
+    }
+
+    if (copyLength < 0)
+        copyLength = src.calcLength();
+
+    if (copyLength >= mBufferSize - at)
+    {
+        SEAD_ASSERT(false, "Buffer overflow. (Buffer Size: %d, At: %d, Copy Length: %d)",
+                    mBufferSize, at, copyLength);
+        copyLength = mBufferSize - at - 1;
+    }
+
+    if (copyLength <= 0)
+        return 0;
+
+    MemUtil::copy(dst + at, src.cstr(), copyLength * sizeof(T));
+    if (at + copyLength > len)
+        dst[at + copyLength] = SafeStringBase<T>::cNullChar;
+
+    return copyLength;
+}
+
+template <typename T>
+inline s32 BufferedSafeStringBase<T>::cutOffCopy(const SafeStringBase<T>& src, s32 copyLength)
 {
     T* dst = getMutableStringTop_();
     const T* csrc = src.cstr();
@@ -111,7 +452,8 @@ inline s32 BufferedSafeStringBase<T>::copy(const SafeStringBase<T>& src, s32 cop
 }
 
 template <typename T>
-inline s32 BufferedSafeStringBase<T>::copyAt(s32 at, const SafeStringBase<T>& src, s32 copyLength)
+inline s32 BufferedSafeStringBase<T>::cutOffCopyAt(s32 at, const SafeStringBase<T>& src,
+                                                   s32 copyLength)
 {
     T* dst = getMutableStringTop_();
     s32 len = this->calcLength();
@@ -126,7 +468,7 @@ inline s32 BufferedSafeStringBase<T>::copyAt(s32 at, const SafeStringBase<T>& sr
     if (copyLength < 0)
         copyLength = src.calcLength();
 
-    if (at + copyLength >= mBufferSize)
+    if (copyLength >= mBufferSize - at)
         copyLength = mBufferSize - at - 1;
 
     if (copyLength <= 0)
