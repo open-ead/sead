c236bdf7672a1e0279087889c1602d6aafa6c77d
Léo Lam <leo@leolam.fr>
2020-07-24 11:59:41 +0200

    Implement Random and GlobalRandom

---
+++ include/random/seadGlobalRandom.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "heap/seadDisposer.h"
+#include "random/seadRandom.h"
+
+namespace sead
+{
+class GlobalRandom : public Random
+{
+    SEAD_SINGLETON_DISPOSER(GlobalRandom)
+};
+}  // namespace sead
