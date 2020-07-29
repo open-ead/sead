859a36bdcc939c158300ecf78dd7a62e0a7120a0
Léo Lam <leo@leolam.fr>
2020-07-29 21:52:43 +0200

    container/TreeNode: Add more functions

--- include/container/seadTreeNode.h
+++ include/container/seadTreeNode.h
@@ -10,7 +10,6 @@ class TreeNode
 public:
     TreeNode();
 
-    void clearChildLinksRecursively_();
     void clearLinks();
     s32 countChildren() const;
     void detachAll();
@@ -23,6 +22,9 @@ public:
     void pushBackSibling(TreeNode*);
     void pushFrontChild(TreeNode*);
 
+protected:
+    void clearChildLinksRecursively_();
+
     TreeNode* mParent;
     TreeNode* mChild;
     TreeNode* mNext;
@@ -33,6 +35,24 @@ template <typename T>
 class TTreeNode : public TreeNode
 {
 public:
+    T& value() { return mData; }
+    const T& value() const { return mData; }
+
+    TTreeNode* parent() const { return static_cast<TTreeNode*>(mParent); }
+    TTreeNode* child() const { return static_cast<TTreeNode*>(mChild); }
+    TTreeNode* next() const { return static_cast<TTreeNode*>(mNext); }
+    TTreeNode* prev() const { return static_cast<TTreeNode*>(mPrev); }
+    TTreeNode* findRoot() { return static_cast<TTreeNode*>(TreeNode::findRoot()); }
+    const TTreeNode* findRoot() const { return static_cast<TTreeNode*>(TreeNode::findRoot()); }
+    void insertAfterSelf(TTreeNode* node) { TreeNode::insertAfterSelf(node); }
+    void insertBeforeSelf(TTreeNode* node) { TreeNode::insertBeforeSelf(node); }
+    void pushBackChild(TTreeNode* node) { TreeNode::pushBackChild(node); }
+    void pushBackSibling(TTreeNode* node) { TreeNode::pushBackSibling(node); }
+    void pushFrontChild(TTreeNode* node) { TreeNode::pushFrontChild(node); }
+
+    // TODO: probably iterators
+
+protected:
     T mData;
 };
 
