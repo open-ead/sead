931c220bc868cde6654d06c670540da6cf6388d2
Léo Lam <leo@leolam.fr>
2020-08-02 20:23:47 +0200

    thread: Finish DelegateThread
    Closes #27

--- include/thread/seadDelegateThread.h
+++ include/thread/seadDelegateThread.h
@@ -1,21 +1,25 @@
-#ifndef SEAD_DELEGATE_THREAD_H_
-#define SEAD_DELEGATE_THREAD_H_
+#pragma once
 
+#include <thread/seadMessageQueue.h>
 #include <thread/seadThread.h>
 
 namespace sead
 {
-template <typename T, typename U>
+template <typename A1, typename A2>
 class IDelegate2;
 
 class DelegateThread : public Thread
 {
 public:
-    virtual ~DelegateThread();
+    DelegateThread(const SafeString& name, IDelegate2<Thread*, MessageQueue::Element>* delegate,
+                   Heap* heap, s32 priority, MessageQueue::BlockType block_type,
+                   MessageQueue::Element quit_msg, s32 stack_size, s32 message_queue_size);
+    ~DelegateThread() override;
 
-    IDelegate2<Thread*, s32>* mDelegate;  // IDelegate2<Thread*, MessageQueue::Element>*
+protected:
+    void calc_(MessageQueue::Element msg) override;
+
+    IDelegate2<Thread*, MessageQueue::Element>* mDelegate;
 };
 
 }  // namespace sead
-
-#endif  // SEAD_DELEGATE_THREAD_H_
