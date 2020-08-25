265046703f4be89eb4c2aa0146d5f8acfa13f49d
Léo Lam <leo@leolam.fr>
2020-08-26 00:50:11 +0200

    resource: Update ArchiveRes to match Switch sead

--- include/resource/seadArchiveRes.h
+++ include/resource/seadArchiveRes.h
@@ -2,6 +2,7 @@
 #define SEAD_ARCHIVE_RES_H_
 
 #include <basis/seadTypes.h>
+#include <container/seadSafeArray.h>
 #include <filedevice/seadFileDevice.h>
 #include <heap/seadHeap.h>
 #include <prim/seadSafeString.h>
@@ -25,23 +26,23 @@ public:
 
 public:
     ArchiveRes() : DirectResource(), mEnable(false) {}
-
     ~ArchiveRes() override = default;
 
-    virtual void doPostCreate_();
     s32 getLoadDataAlignment() const override;
     void doCreate_(u8* buf, u32, Heap*) override;
-    virtual void* getFileImpl_(const SafeString& file_path, FileInfo* file_info = NULL) = 0;
-    virtual void* getFileFastImpl_(s32 entry_id, FileInfo* file_info) = 0;
-    virtual s32 convertPathToEntryIDImpl_(const SafeString& file_path) = 0;
+
+protected:
+    using Handle = SafeArray<u8, 32>;
+
+    virtual void* getFileImpl_(const SafeString& file_path,
+                               FileInfo* file_info = nullptr) const = 0;
+    virtual void* getFileFastImpl_(s32 entry_id, FileInfo* file_info) const = 0;
+    virtual s32 convertPathToEntryIDImpl_(const SafeString& file_path) const = 0;
     virtual bool setCurrentDirectoryImpl_(const SafeString&) = 0;
-    virtual bool openDirectoryImpl_(
-        u32* handle,
-        const SafeString&) = 0;  // openDirectoryImpl_(SafeArray<u8, 32>*, const SafeString&)
-    virtual bool closeDirectoryImpl_(u32* handle) = 0;  // closeDirectoryImpl_(SafeArray<u8, 32>*)
-    virtual u32 readDirectoryImpl_(
-        u32* handle, DirectoryEntry* entry,
-        u32 num) = 0;  // readDirectoryImpl_(SafeArray<u8, 32>*, DirectoryEntry*, u32)
+    virtual bool openDirectoryImpl_(Handle* handle, const SafeString&) const = 0;
+    virtual bool closeDirectoryImpl_(Handle* handle) const = 0;
+    virtual u32 readDirectoryImpl_(Handle* handle, DirectoryEntry* entries, u32 num) const = 0;
+    virtual bool isExistFileImpl_(const SafeString& path) const;
     virtual bool prepareArchive_(const void* archive) = 0;
 
     bool mEnable;
--- modules/src/resource/seadArchiveRes.cpp
+++ modules/src/resource/seadArchiveRes.cpp
@@ -2,8 +2,6 @@
 
 namespace sead
 {
-void ArchiveRes::doPostCreate_() {}
-
 s32 ArchiveRes::getLoadDataAlignment() const
 {
     return 0x80;
@@ -14,4 +12,8 @@ void ArchiveRes::doCreate_(u8* buf, u32, Heap*)
     mEnable = prepareArchive_(buf);
 }
 
+bool ArchiveRes::isExistFileImpl_(const SafeString& path) const
+{
+    return convertPathToEntryIDImpl_(path) != -1;
+}
 }  // namespace sead
