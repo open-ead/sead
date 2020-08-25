d2b3788560ebd236775698a495145822c056e0fb
Léo Lam <leo@leolam.fr>
2020-08-25 15:19:37 +0200

    filedevice: Implement NinHostIOFileDevice

--- include/devenv/seadEnvUtil.h
+++ include/devenv/seadEnvUtil.h
@@ -8,5 +8,6 @@ class EnvUtil
 {
 public:
     static s32 getEnvironmentVariable(BufferedSafeString* out, const SafeString& variable);
+    static s32 resolveEnvronmentVariable(BufferedSafeString* out, const SafeString& str);
 };
 }  // namespace sead
