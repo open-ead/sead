76787f1baed02c9528bafeb5831a2d03862bda74
Léo Lam <leo@leolam.fr>
2020-08-27 01:12:00 +0200

    filedevice: Implement ArchiveFileDevice

--- include/filedevice/seadArchiveFileDevice.h
+++ include/filedevice/seadArchiveFileDevice.h
@@ -4,23 +4,31 @@
 
 namespace sead
 {
+class ArchiveRes;
+
 class ArchiveFileDevice : public FileDevice
 {
     SEAD_RTTI_OVERRIDE(ArchiveFileDevice, FileDevice)
 public:
-    using FileDevice::FileDevice;
+    explicit ArchiveFileDevice(ArchiveRes* archive_res);
     ~ArchiveFileDevice() override = default;
 
+    u8* tryLoadWithEntryID(s32 id, LoadArg& arg);
+    FileDevice* tryOpenWithEntryID(FileHandle* handle, s32 id, FileOpenFlag flag, u32 div_size);
+    s32 tryConvertPathToEntryID(const SafeString& path);
+    bool setCurrentDirectory(const SafeString& dir);
+
 protected:
-    bool doIsAvailable_() const override;
+    struct ArchiveFileHandle;
+
+    bool doIsAvailable_() const override { return true; }
     u8* doLoad_(LoadArg& arg) override;
     FileDevice* doOpen_(FileHandle* handle, const SafeString& path, FileOpenFlag flag) override;
     bool doClose_(FileHandle* handle) override;
     bool doFlush_(FileHandle* handle) override;
     bool doRemove_(const SafeString& str) override;
     bool doRead_(u32* bytesRead, FileHandle* handle, u8* outBuffer, u32 bytesToRead) override;
-    bool doWrite_(u32* bytesWritten, FileHandle* handle, const u8* inBuffer,
-                  u32 bytesToWrite) override;
+    bool doWrite_(u32*, FileHandle*, const u8*, u32) override { return false; }
     bool doSeek_(FileHandle* handle, s32 offset, SeekOrigin origin) override;
     bool doGetCurrentSeekPos_(u32* seekPos, FileHandle* handle) override;
     bool doGetFileSize_(u32* fileSize, const SafeString& path) override;
@@ -29,14 +37,19 @@ protected:
     bool doIsExistDirectory_(bool* exists, const SafeString& path) override;
     FileDevice* doOpenDirectory_(DirectoryHandle* handle, const SafeString& path) override;
     bool doCloseDirectory_(DirectoryHandle* handle) override;
-    bool doReadDirectory_(u32* entriesRead, DirectoryHandle* handle, DirectoryEntry* entries,
+    bool doReadDirectory_(u32* entriesRead, DirectoryHandle* handle, DirectoryEntry* entry,
                           u32 entriesToRead) override;
     bool doMakeDirectory_(const SafeString& path, u32 u_32) override;
     s32 doGetLastRawError_() const override;
 
-    u8* doLoadWithEntryID_(s32 id, LoadArg& arg);
-    FileDevice* doOpenWithEntryID_(FileHandle* handle, s32 id, FileOpenFlag flag);
-    s32 doConvertPathToEntryID_(const SafeString& path);
-    bool doSetCurrentDirectory_(const SafeString& path);
+    virtual u8* doLoadWithEntryID_(s32 id, LoadArg& arg);
+    virtual FileDevice* doOpenWithEntryID_(FileHandle* handle, s32 id, FileOpenFlag flag);
+    virtual s32 doConvertPathToEntryID_(const SafeString& path);
+    virtual bool doSetCurrentDirectory_(const SafeString& path);
+
+    ArchiveFileHandle* getArchiveFileHandle_(FileHandle* handle) const;
+    ArchiveFileHandle* constructArchiveFileHandle_(FileHandle* handle) const;
+
+    ArchiveRes* mArchive;
 };
 }  // namespace sead
--- include/resource/seadArchiveRes.h
+++ include/resource/seadArchiveRes.h
@@ -1,6 +1,7 @@
 #ifndef SEAD_ARCHIVE_RES_H_
 #define SEAD_ARCHIVE_RES_H_
 
+#include <basis/seadRawPrint.h>
 #include <basis/seadTypes.h>
 #include <container/seadSafeArray.h>
 #include <filedevice/seadFileDevice.h>
@@ -15,11 +16,8 @@ class ArchiveRes : public DirectResource
     SEAD_RTTI_OVERRIDE(ArchiveRes, DirectResource)
 
 public:
-    class FileInfo
+    struct FileInfo
     {
-    public:
-        explicit FileInfo() : mStartOffset(0), mLength(0) {}
-
         u32 mStartOffset;
         u32 mLength;
     };
@@ -31,15 +29,54 @@ public:
     s32 getLoadDataAlignment() const override;
     void doCreate_(u8* buf, u32, Heap*) override;
 
+    const void* getFile(const SafeString& file_path, FileInfo* info = nullptr) const
+    {
+        SEAD_ASSERT(mEnable);
+        return getFileImpl_(file_path, info);
+    }
+
+    const void* getFileFast(const s32 entry_id, FileInfo* info) const
+    {
+        SEAD_ASSERT(mEnable);
+        return getFileFastImpl_(entry_id, info);
+    }
+
+    s32 convertPathToEntryID(const SafeString& path) const
+    {
+        SEAD_ASSERT(mEnable);
+        return convertPathToEntryIDImpl_(path);
+    }
+
+    bool setCurrentDirectory(const SafeString& dir)
+    {
+        SEAD_ASSERT(mEnable);
+        return setCurrentDirectoryImpl_(dir);
+    }
+
+    bool openDirectory(HandleBuffer* handle, const SafeString& dir) const
+    {
+        return openDirectoryImpl_(handle, dir);
+    }
+
+    bool closeDirectory(HandleBuffer* handle) const { return closeDirectoryImpl_(handle); }
+
+    u32 readDirectory(HandleBuffer* handle, DirectoryEntry* entries, u32 num) const
+    {
+        return readDirectoryImpl_(handle, entries, num);
+    }
+
+    bool isExistFile(const SafeString& path) const { return isExistFileImpl_(path); }
+
 protected:
     virtual const void* getFileImpl_(const SafeString& file_path,
-                               FileInfo* file_info = nullptr) const = 0;
+                                     FileInfo* file_info = nullptr) const = 0;
     virtual const void* getFileFastImpl_(s32 entry_id, FileInfo* file_info) const = 0;
     virtual s32 convertPathToEntryIDImpl_(const SafeString& file_path) const = 0;
     virtual bool setCurrentDirectoryImpl_(const SafeString&) = 0;
     virtual bool openDirectoryImpl_(HandleBuffer* handle, const SafeString& path) const = 0;
     virtual bool closeDirectoryImpl_(HandleBuffer* handle) const = 0;
-    virtual u32 readDirectoryImpl_(HandleBuffer* handle, DirectoryEntry* entries, u32 num) const = 0;
+    virtual u32 readDirectoryImpl_(HandleBuffer* handle, DirectoryEntry* entries,
+                                   u32 num) const = 0;
     virtual bool isExistFileImpl_(const SafeString& path) const;
     virtual bool prepareArchive_(const void* archive) = 0;
 
