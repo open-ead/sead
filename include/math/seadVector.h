706e4828350cc9108d6c64cb118bd3e503fa9708
Léo Lam <leo@leolam.fr>
2020-08-29 21:14:12 +0200

    math/Vector: Add common, useful operators (e.g. +, -, *)

--- include/math/seadVector.h
+++ include/math/seadVector.h
@@ -9,12 +9,56 @@ namespace sead
 template <typename T>
 struct Vector2 : public Policies<T>::Vec2Base
 {
-    Vector2() {}
-    Vector2(T x, T y)
+    constexpr Vector2() = default;
+    constexpr Vector2(T x, T y)
     {
         this->x = x;
         this->y = y;
     }
+
+    Vector2& operator+=(const Vector2& other)
+    {
+        this->x += other.x;
+        this->y += other.y;
+        return *this;
+    }
+
+    friend Vector2 operator+(const Vector2& a, const Vector2& b)
+    {
+        return {a.x + b.x, a.y + b.y};
+    }
+
+    Vector2& operator-=(const Vector2& other)
+    {
+        this->x -= other.x;
+        this->y -= other.y;
+        return *this;
+    }
+
+    friend Vector2 operator-(const Vector2& a, const Vector2& b)
+    {
+        return {a.x - b.x, a.y - b.y};
+    }
+
+    Vector2& operator*=(T t)
+    {
+        this->x *= t;
+        this->y *= t;
+        return *this;
+    }
+
+    friend Vector2 operator*(const Vector2& a, T t) { return {a.x * t, a.y * t}; }
+    friend Vector2 operator*(T t, const Vector2& a) { return operator*(a, t); }
+
+    Vector2& operator/=(T t)
+    {
+        this->x /= t;
+        this->y /= t;
+        return *this;
+    }
+
+    friend Vector2 operator/(const Vector2& a, T t) { return {a.x / t, a.y / t}; }
+    friend Vector2 operator/(T t, const Vector2& a) { return operator/(a, t); }
 
     static const Vector2 zero;
     static const Vector2 ex;
@@ -25,13 +69,61 @@ struct Vector2 : public Policies<T>::Vec2Base
 template <typename T>
 struct Vector3 : public Policies<T>::Vec3Base
 {
-    Vector3() {}
-    Vector3(T x, T y, T z)
+    constexpr Vector3() = default;
+    constexpr Vector3(T x, T y, T z)
     {
         this->x = x;
         this->y = y;
         this->z = z;
     }
+
+    Vector3& operator+=(const Vector3& other)
+    {
+        this->x += other.x;
+        this->y += other.y;
+        this->z += other.z;
+        return *this;
+    }
+
+    friend Vector3 operator+(const Vector3& a, const Vector3& b)
+    {
+        return {a.x + b.x, a.y + b.y, a.z + b.z};
+    }
+
+    Vector3& operator-=(const Vector3& other)
+    {
+        this->x -= other.x;
+        this->y -= other.y;
+        this->z -= other.z;
+        return *this;
+    }
+
+    friend Vector3 operator-(const Vector3& a, const Vector3& b)
+    {
+        return {a.x - b.x, a.y - b.y, a.z - b.z};
+    }
+
+    Vector3& operator*=(T t)
+    {
+        this->x *= t;
+        this->y *= t;
+        this->z *= t;
+        return *this;
+    }
+
+    friend Vector3 operator*(const Vector3& a, T t) { return {a.x * t, a.y * t, a.z * t}; }
+    friend Vector3 operator*(T t, const Vector3& a) { return operator*(a, t); }
+
+    Vector3& operator/=(T t)
+    {
+        this->x /= t;
+        this->y /= t;
+        this->z /= t;
+        return *this;
+    }
+
+    friend Vector3 operator/(const Vector3& a, T t) { return {a.x / t, a.y / t, a.z / t}; }
+    friend Vector3 operator/(T t, const Vector3& a) { return operator/(a, t); }
 
     static const Vector3 zero;
     static const Vector3 ex;
@@ -43,14 +135,66 @@ struct Vector3 : public Policies<T>::Vec3Base
 template <typename T>
 struct Vector4 : public Policies<T>::Vec4Base
 {
-    Vector4() {}
-    Vector4(T x, T y, T z, T w)
+    constexpr Vector4() = default;
+    constexpr Vector4(T x, T y, T z, T w)
     {
         this->x = x;
         this->y = y;
         this->z = z;
         this->w = w;
     }
+
+    Vector4& operator+=(const Vector4& other)
+    {
+        this->x += other.x;
+        this->y += other.y;
+        this->z += other.z;
+        this->w += other.w;
+        return *this;
+    }
+
+    friend Vector4 operator+(const Vector4& a, const Vector4& b)
+    {
+        return {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+    }
+
+    Vector4& operator-=(const Vector4& other)
+    {
+        this->x -= other.x;
+        this->y -= other.y;
+        this->z -= other.z;
+        this->w -= other.w;
+        return *this;
+    }
+
+    friend Vector4 operator-(const Vector4& a, const Vector4& b)
+    {
+        return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
+    }
+
+    Vector4& operator*=(T t)
+    {
+        this->x *= t;
+        this->y *= t;
+        this->z *= t;
+        this->w *= t;
+        return *this;
+    }
+
+    friend Vector4 operator*(const Vector4& a, T t) { return {a.x * t, a.y * t, a.z * t, a.w * t}; }
+    friend Vector4 operator*(T t, const Vector4& a) { return operator*(a, t); }
+
+    Vector4& operator/=(T t)
+    {
+        this->x /= t;
+        this->y /= t;
+        this->z /= t;
+        this->w /= t;
+        return *this;
+    }
+
+    friend Vector4 operator/(const Vector4& a, T t) { return {a.x / t, a.y / t, a.z / t, a.w / t}; }
+    friend Vector4 operator/(T t, const Vector4& a) { return operator/(a, t); }
 
     static const Vector4 zero;
     static const Vector4 ex;
