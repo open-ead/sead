207ccfb3f530a0e329c30791c290ec274c52bf4f
Léo Lam <leo@leolam.fr>
2021-08-01 22:38:55 +0200

    math/Quat: Fix matching issues (notably in invert)

--- include/math/seadQuat.h
+++ include/math/seadQuat.h
@@ -2,6 +2,7 @@
 
 #include <basis/seadTypes.h>
 #include <cmath>
+#include <limits>
 #include <math/seadMathPolicies.h>
 
 namespace sead
@@ -10,33 +11,50 @@ template <typename T>
 struct Quat : public Policies<T>::QuatBase
 {
     Quat() {}
-    Quat(const Quat& other) = default;
-    Quat(T x, T y, T z, T w)
+    Quat(const Quat& other) { *this = other; }
+    Quat(T x, T y, T z, T w) { set(x, y, z, w); }
+
+    void set(T x, T y, T z, T w)
     {
+        this->w = w;
         this->x = x;
         this->y = y;
         this->z = z;
-        this->w = w;
     }
 
     constexpr Quat& operator=(const Quat& other)
     {
+        this->w = other.w;
         this->x = other.x;
         this->y = other.y;
         this->z = other.z;
-        this->w = other.w;
         return *this;
     }
 
     constexpr friend Quat operator*(const Quat& a, T t)
     {
-        return {a.x * t, a.y * t, a.z * t, a.w * t};
+        auto result = a;
+        result *= t;
+        return result;
     }
+
     constexpr friend Quat operator*(const Quat& a, const Quat& b)
     {
-        return {a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w};
+        auto result = a;
+        result *= b;
+        return result;
     }
+
     constexpr friend Quat operator*(T t, const Quat& a) { return operator*(a, t); }
+
+    constexpr Quat& operator*=(const Quat& t)
+    {
+        this->w *= t.w;
+        this->x *= t.x;
+        this->y *= t.y;
+        this->z *= t.z;
+        return *this;
+    }
 
     constexpr Quat& operator*=(T t)
     {
@@ -47,12 +65,13 @@ struct Quat : public Policies<T>::QuatBase
         return *this;
     }
 
-    static const Quat unit;
     void normalize();
-    Quat inverse();
+    void invert(Quat* inverse);
+
+    static const Quat unit;
 };
 
-typedef Quat<f32> Quatf;
+using Quatf = Quat<f32>;
 
 template <>
 const Quatf Quatf::unit;
@@ -69,28 +88,33 @@ inline T norm2(const Quat<T>& v)
     return std::sqrt(dot(v, v));
 }
 
-//NON_MATCHING: Order of parts unknown. Used in `physSupportBoneResource.cpp` - SupportBoneResource::postRead_
 template <typename T>
 inline void Quat<T>::normalize()
 {
     const auto norm = norm2(*this);
-    if(norm > 0.0){
-        T fac = T(1) / norm;
-        this->w *= fac;
-        this->z *= fac;
-        this->x *= fac;
-        this->y *= fac;
-    }
+    if (norm > 0.0)
+        *this *= T(1) / norm;
 }
 
 template <typename T>
-inline Quat<T> Quat<T>::inverse()
+inline void Quat<T>::invert(Quat* inverse)
 {
-    auto inverse = *this;
     T prod = dot(*this, *this);
     if (prod > std::numeric_limits<T>::epsilon())
-        inverse *= T(1) / prod;
-    return {-inverse.x, -inverse.y, -inverse.z, inverse.w};
+    {
+        T inv = T(1) / prod;
+        inverse->w = inv * this->w;
+        inverse->x = inv * -this->x;
+        inverse->y = inv * -this->y;
+        inverse->z = inv * -this->z;
+    }
+    else
+    {
+        inverse->w = this->w;
+        inverse->x = -this->x;
+        inverse->y = -this->y;
+        inverse->z = -this->z;
+    }
 }
 
 }  // namespace sead
