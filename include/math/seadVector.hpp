9b42350bc776594de9547fbab2a2fbfd2d72b182
ThePixelGamer <thepixelmaster2354@gmail.com>
2021-12-19 11:41:37 -0600

    math: Fix friend functions and add Vec3 negate

--- include/math/seadVector.hpp
+++ include/math/seadVector.hpp
@@ -14,34 +14,6 @@ inline Vector2<T>::Vector2(T x_, T y_)
 }
 
 template <typename T>
-inline Vector2<T> operator+(const Vector2<T>& a, const Vector2<T>& b)
-{
-    Vector2<T> o;
-    Vector2CalcCommon<T>::add(o, a, b);
-    return o;
-}
-
-template <typename T>
-inline Vector2<T> operator-(const Vector2<T>& a, const Vector2<T>& b)
-{
-    Vector2<T> o;
-    Vector2CalcCommon<T>::sub(o, a, b);
-    return o;
-}
-
-template <typename T>
-inline Vector2<T> operator*(T t, const Vector2<T>& a)
-{
-    return operator*(a, t);
-}
-
-template <typename T>
-inline Vector2<T> operator/(const Vector2<T>& a, T t)
-{
-    return {a.x / t, a.y / t};
-}
-
-template <typename T>
 inline Vector2<T>& Vector2<T>::operator+=(const Vector2<T>& other)
 {
     Vector2CalcCommon<T>::add(*this, *this, other);
@@ -94,20 +66,6 @@ template <typename T>
 inline Vector3<T>::Vector3(T x_, T y_, T z_)
 {
     Vector3CalcCommon<T>::set(*this, x_, y_, z_);
-}
-
-template <typename T>
-inline Vector3<T> operator+(const Vector3<T>& a, const Vector3<T>& b)
-{
-    Vector3<T> o;
-    Vector3CalcCommon<T>::add(o, a, b);
-    return o;
-}
-
-template <typename T>
-inline Vector3<T> operator/(const Vector3<T>& a, T t)
-{
-    return {a.x / t, a.y / t, a.z / t};
 }
 
 template <typename T>
@@ -205,30 +163,6 @@ template <typename T>
 inline Vector4<T>::Vector4(T x_, T y_, T z_, T w_)
 {
     Vector4CalcCommon<T>::set(*this, x_, y_, z_, w_);
-}
-
-template <typename T>
-inline Vector4<T> operator+(const Vector4<T>& a, const Vector4<T>& b)
-{
-    return {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
-}
-
-template <typename T>
-inline Vector4<T> operator-(const Vector4<T>& a, const Vector4<T>& b)
-{
-    return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
-}
-
-template <typename T>
-inline Vector4<T> operator*(T t, const Vector4<T>& a)
-{
-    return operator*(a, t);
-}
-
-template <typename T>
-inline Vector4<T> operator/(const Vector4<T>& a, T t)
-{
-    return {a.x / t, a.y / t, a.z / t, a.w / t};
 }
 
 template <typename T>
