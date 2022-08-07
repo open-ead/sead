0f94b9af7b6ccee82f3cde4b95e420f09265425f
Léo Lam <leo@leolam.fr>
2022-08-07 23:18:29 +0100

    prim/ScopedLock: Fix makeScopedLock for pre-C++17 builds
    Make ScopedLock and ConditionalScopedLock movable types because
    pre-C++17 code cannot benefit from copy/move elision.

--- include/prim/seadScopedLock.h
+++ include/prim/seadScopedLock.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 namespace sead
 {
 template <typename T>
@@ -7,18 +9,36 @@ class ScopedLock
 {
 public:
     explicit ScopedLock(T* lock) : mLocked(lock) { mLocked->lock(); }
+
     ScopedLock(const ScopedLock& other) = delete;
-    const ScopedLock& operator=(const ScopedLock& other) = delete;
-    virtual ~ScopedLock() { mLocked->unlock(); }
+    ScopedLock& operator=(const ScopedLock& other) = delete;
+
+    ScopedLock(ScopedLock&& other) noexcept { *this = std::move(other); }
+    ScopedLock& operator=(ScopedLock&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        mLocked = other.mLocked;
+        mEngaged = std::exchange(other.mEngaged, false);
+        return *this;
+    }
+
+    virtual ~ScopedLock()
+    {
+        if (mEngaged)
+            mLocked->unlock();
+    }
 
 protected:
+    bool mEngaged = true;
     T* mLocked;
 };
 
 template <typename T>
 [[nodiscard]] inline ScopedLock<T> makeScopedLock(T& lock)
 {
-    return ScopedLock<T>(&lock);
+    return ScopedLock<T>{&lock};
 }
 
 template <typename T>
@@ -32,15 +52,29 @@ public:
         mLocked = lock;
         mLocked->lock();
     }
+
     ConditionalScopedLock(const ConditionalScopedLock& other) = delete;
-    const ConditionalScopedLock& operator=(const ConditionalScopedLock& other) = delete;
+    ConditionalScopedLock& operator=(const ConditionalScopedLock& other) = delete;
+
+    ConditionalScopedLock(ConditionalScopedLock&& other) noexcept { *this = std::move(other); }
+    ConditionalScopedLock& operator=(ConditionalScopedLock&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        mLocked = other.mLocked;
+        mEngaged = std::exchange(other.mEngaged, false);
+        return *this;
+    }
+
     virtual ~ConditionalScopedLock()
     {
-        if (mLocked)
+        if (mEngaged && mLocked)
             mLocked->unlock();
     }
 
 protected:
+    bool mEngaged = true;
     T* mLocked = nullptr;
 };
 
