3c680c59fd941543eb8ec2212ef605c58fa9934a
MonsterDruide1 <5958456@gmail.com>
2022-08-09 16:08:23 +0200

    gfx/TextWriter: Add all listed functions to header

--- include/gfx/seadTextWriter.h
+++ include/gfx/seadTextWriter.h
@@ -9,20 +9,42 @@ class Viewport;
 class Camera;
 class Projection;
 class DrawContext;
+class FontBase;
+class DebugFontMgrNvn;
 
 class TextWriter
 {
 public:
+    TextWriter(DrawContext*);
+    TextWriter(DrawContext*, const Viewport*);
     virtual ~TextWriter();
 
-    void printImpl_(const char*, int, bool, BoundBox2f*);
-    void printf(const char* format, ...);
-    void printf(const char16_t* format, ...);
-    void setScaleFromFontHeight(float scale);
+    DebugFontMgrNvn* getDefaultFont();
+    static void setDefaultFont(FontBase*);
+    static void setupGraphics(DrawContext*);
+    void getCursorFromTopLeft(Vector2f*);
     void setCursorFromTopLeft(const Vector2f& pos);
+    void setScaleFromFontSize(const Vector2f&);
+    void setScaleFromFontHeight(float scale);
+    void setProjectionAndCamera(const Projection*, const Camera*);
+    void setLineSpaceFromLineHeight(float);
+    void setFormatBuffer(char16_t*, int);
+    void setDrawContext(DrawContext*);
     void beginDraw();
-    static void setupGraphics(DrawContext* context);
+    void endDraw();
+    void printf(const char16_t* format, ...);
+    void vprintfImpl_(const char16_t*, std::va_list, bool, BoundBox2f*);
+    void printfWithCalcRect(BoundBox2f*, const char16_t*, ...);
+    void printf(const char* format, ...);
+    void vprintfImpl_(const char*, std::va_list, bool, BoundBox2f*);
+    void printfWithCalcRect(BoundBox2f*, const char*, ...);
+    void calcFormatStringRect(BoundBox2f*, const char16_t*, ...);
+    void calcFormatStringRect(BoundBox2f*, const char*, ...);
+    void printImpl_(const char16_t*, int, bool, BoundBox2f*, const Projection*, const Camera*);
+    void printImpl_(const char16_t*, int, bool, BoundBox2f*);
+    void printImpl_(const char*, int, bool, BoundBox2f*);
 
+private:
     Viewport* mViewport;
     Projection* mProjection;
     Camera* mCamera;
@@ -35,8 +57,8 @@ public:
     int _48;
     float mLineSpace;
     BoundBox2f mBoundBox2;
-    int TextWriter_x58;
-    int TextWriter_x5C;
+    float _58;
+    int _5c;
     char16_t* mFormatBuffer;
     int mFormatBufferSize;
     int _6c;
