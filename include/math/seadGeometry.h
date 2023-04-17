e473dc2f446c8c7758230b0d8bb9d15af7d75f2d
AboodXD <aboood40091@gmail.com>
2023-04-17 18:17:13 +0400

    engine/geom: Add Segment

--- include/math/seadGeometry.h
+++ include/math/seadGeometry.h
@@ -1,28 +1,17 @@
 #pragma once
 
+#include <geom/seadLine.h>
 #include <math/seadVector.h>
 
 namespace sead
 {
-
-template <typename T>
-struct Ray
-{
-    Ray();
-
-    T position;
-    T direction;
-};
-
 namespace Geometry
 {
-float calcSquaredDistancePointToRay(Vector2<float>* point, Ray<Vector2<float>>* ray, float* scalar);
-// float calcSquaredDistancePointToLine(Vector2<float>* const, Line<sead::Vector2<float>>* const,
-// float*); float calcSquaredDistancePointToSegment(Vector3<float>* const, Segment<Vector3<float>>*
-// const, float*); float calcSquaredDistancePointToSegment();
-// float calcSquaredDistanceSegmentToSegment(Segment<Vector2<float>>* const,
-//                                           Segment<Vector2<float>>* const, float*, float*);
-// float calcSquaredDistanceRayToSegment(Ray<Vector2<float>> const&, Segment<Vector2<float>>* const,
-//                                       float*, float*);
+float calcSquaredDistancePointToRay(Vector2f* point, Ray2f* ray, float* scalar);
+// float calcSquaredDistancePointToLine(Vector2f* const, Line2f* const, float*);
+// float calcSquaredDistancePointToSegment(Vector3f* const, Segment3f* const, float*);
+// float calcSquaredDistancePointToSegment();
+// float calcSquaredDistanceSegmentToSegment(Segment2f* const, Segment2f* const, float*, float*);
+// float calcSquaredDistanceRayToSegment(Ray2f const&, Segment2f* const, float*, float*);
 }  // namespace Geometry
 }  // namespace sead
