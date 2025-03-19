867ed8e8744021eea321b01aa6ec4997742cf7dc
tetraxile <64417119+tetraxile@users.noreply.github.com>
2025-03-19 17:46:54 +0000

    framework: fill out more of `GameFrameworkNx.h` (#165)

--- include/framework/nx/seadGameFrameworkNx.h
+++ include/framework/nx/seadGameFrameworkNx.h
@@ -15,7 +15,8 @@ class StandardAllocator;
 namespace vi
 {
 class Layer;
-}
+class Display;
+}  // namespace vi
 }  // namespace nn
 
 namespace sead
@@ -29,13 +30,45 @@ public:
     struct CreateArg
     {
         s32 mVBlankWaitInterval;
-        char _8[68];
+        Color4f mClearColor;
+        Vector2u mDisplayResolution;
+        bool _1c;
+        bool mIsTripleBuffer;
+        bool mIsUseDebug;
+        bool _1f;
+        u32 _20;
+        u32 _24;
+        u32 _28;
+        s32 mTransferMemorySize;
+        s32 mGraphicsDevToolsMemorySize;
+        s32 mComputeMemorySize;
+        s32 mCommandMemorySize;
+        s32 mControlMemorySize;
+        u32 mDebugLevel;
+        u32 _44;
     };
 
     static_assert(sizeof(CreateArg) == 0x48);
 
     GameFrameworkNx(const CreateArg&);
     ~GameFrameworkNx() override;
+
+    FrameBuffer* getMethodFrameBuffer(int) const override;
+    LogicalFrameBuffer* getMethodLogicalFrameBuffer(int) const override;
+    void initRun_(Heap*) override;
+    void runImpl_() override;
+    MethodTreeMgr* createMethodTreeMgr_(Heap*) override;
+    float calcFps() override;
+    virtual void setCaption(const SafeString&);
+    virtual void mainLoop_();
+    virtual void procFrame_();
+    virtual void procDraw_();
+    virtual void procCalc_();
+    virtual void present_();
+    virtual void swapBuffer_();
+    virtual void clearFrameBuffers_(int);
+    virtual void waitForGpuDone_();
+    virtual void setGpuTimeStamp_();
 
     void initializeArg(const Framework::InitializeArg&);
     void initializeGraphicsSystem(Heap*, const Vector2f&);
@@ -45,37 +78,41 @@ public:
     void setVBlankWaitInterval(u32);
     void requestChangeUseGPU(bool);
     void getGraphicsDevToolsAllocatorTotalFreeSize() const;  // unknown return type
-    void initRun_(Heap*) override;
-    void runImpl_() override;
-    MethodTreeMgr* createMethodTreeMgr_(Heap*) override;
-    void mainLoop_();
-    void procFrame_();
-    void procDraw_();
-    void procCalc_();
-    void present_();
     void waitVsyncEvent_();
-    void swapBuffer_();
-    void clearFrameBuffers_(int);
-    void waitForGpuDone_();
-    void setGpuTimeStamp_();  // unknown return type
-    FrameBuffer* getMethodFrameBuffer(int) const override;
-    LogicalFrameBuffer* getMethodLogicalFrameBuffer(int) const override;
-    float calcFps();
-    void setCaption(const SafeString&);
 
 private:
     CreateArg mCreateArg;
-    char _E8[8];
-    u64 mSystemTick;
+    u64 mFrameDuration;
+    u64 mPrevFrameTick;
     FrameBuffer* mMethodFrameBuffer;
     LogicalFrameBuffer mMethodLogicalFrameBuffer;
-    char _120[8];
+    u64 mVBlankWaitTicks;
     DisplayBufferNvn* mDisplayBuffer;
-    char _130[72];
+    char _130[8];
+    void* mGpuErrorCallback;
+    NVNmemoryPool* _140;
+    void* mCommandBufferControl;
+    NVNcommandBuffer* mCommandBuffer;
+    NVNbuffer* _158;
+    NVNcounterData* _160;
+    NVNmemoryPool* mShaderScratchMemory;
+    u32 _170;
     nn::mem::StandardAllocator* mGraphicsDevToolsAllocator;
-    char _180[40];
-    nn::vi::Layer* mDisplay;
-    char _1B0[96];
+    u64 mCommandBufferCommandMemoryUsed;
+    u64 mCommandBufferControlMemoryUsed;
+    NVNqueue* _190;
+    FrameBuffer* mCurFrameBuffer;
+    nn::vi::Display* mDisplay;
+    nn::vi::Layer* mLayer;
+    DelegateThread* mPresentationThread;
+    NVNsync* _1B8;
+    SafeString _1C0;
+    NVNcommandHandle mCommandHandle;
+    nn::os::SystemEventType mVsyncEvent;
+    bool _208;
+    u8 _209;
+    bool _20a;
+    u8 _20b;
 };
 
 static_assert(sizeof(GameFrameworkNx) == 0x210);
