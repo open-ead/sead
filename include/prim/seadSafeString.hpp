e0425e635938aa4e0f276050c12627df603bbf03
ud2 <sjx233@qq.com>
2025-04-11 09:05:03 +0800

    prim: fix `BufferedSafeStringBase<T>::append(T)`

--- include/prim/seadSafeString.hpp
+++ include/prim/seadSafeString.hpp
@@ -558,6 +558,24 @@ inline s32 BufferedSafeStringBase<T>::append(const SafeStringBase<T>& str, s32 a
 }
 
 template <typename T>
+inline s32 BufferedSafeStringBase<T>::append(T c)
+{
+    const s32 length = this->calcLength();
+
+    if (length >= getBufferSize() - 1)
+    {
+        SEAD_ASSERT_MSG(false, "Buffer overflow. (Buffer Size: %d, Length: %d)", getBufferSize(),
+                        length);
+        return 0;
+    }
+
+    T* top = getMutableStringTop_();
+    top[length] = c;
+    top[length + 1] = this->cNullChar;
+    return 1;
+}
+
+template <typename T>
 inline s32 BufferedSafeStringBase<T>::append(T c, s32 num)
 {
     if (num < 0)
