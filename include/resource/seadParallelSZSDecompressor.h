b6f820d9e810927ee464ac43f5bb5402b686f560
MonsterDruide1 <5958456@gmail.com>
2025-05-20 23:27:39 +0200

    resource: Add `ParallelSZSDecompressor`

---
+++ include/resource/seadParallelSZSDecompressor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <mc/seadCoreInfo.h>
+#include <resource/seadDecompressor.h>
+
+namespace sead
+{
+
+class ParallelSZSDecompressor : public Decompressor
+{
+public:
+    ParallelSZSDecompressor(u32, s32, sead::Heap*, u8*, const CoreIdMask&);
+    ~ParallelSZSDecompressor() override;
+
+    u8* tryDecompFromDevice(const sead::ResourceMgr::LoadArg&, sead::Resource*, u32*, u32*,
+                            bool*) override;
+
+    void setDivSize(u32);
+
+private:
+    void* size[0x1a8 / 8];
+};
+
+static_assert(sizeof(ParallelSZSDecompressor) == 0x220);
+
+}  // namespace sead
