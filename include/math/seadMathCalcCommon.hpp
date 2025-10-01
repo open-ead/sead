87e85b123afca5b7aaca5ffb847ca44b57261ec3
german77 <5944268+german77@users.noreply.github.com>
2025-09-30 20:34:47 -0600

    math: Fix floor to int

--- include/math/seadMathCalcCommon.hpp
+++ include/math/seadMathCalcCommon.hpp
@@ -441,7 +441,12 @@ inline s32 MathCalcCommon<s32>::roundOff(s32 val)
 template <typename T>
 inline s32 MathCalcCommon<T>::floor(T val)
 {
-    return std::floor(val);
+    s32 x = static_cast<s32>(val);
+
+    if (x == val)
+        return x;
+
+    return x + (val >= 0 ? 0 : -1);
 }
 
 template <>
