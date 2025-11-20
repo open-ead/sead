a02b344794a9453379096fb17bcb2c4d9bfc2c3c
german77 <5944268+german77@users.noreply.github.com>
2025-11-20 12:03:31 -0600

    gfx: Partially implement Projection

--- modules/src/gfx/seadProjection.cpp
+++ modules/src/gfx/seadProjection.cpp
@@ -1,30 +1,84 @@
 #include <gfx/seadProjection.h>
 
+#include <gfx/seadCamera.h>
+#include <gfx/seadViewport.h>
+
 namespace sead
 {
+Projection::Projection()
+{
+    mDevicePosture = Graphics::sDefaultDevicePosture;
+    mDeviceZScale = Graphics::sDefaultDeviceZScale;
+    mDeviceZOffset = Graphics::sDefaultDeviceZOffset;
+}
+
+void Projection::updateAttributesForDirectProjection() {}
+
+const Matrix44f& Projection::getProjectionMatrix() const
+{
+    updateMatrixImpl_();
+    return mMatrix;
+}
+
 void Projection::updateMatrixImpl_() const
 {
     if (mDirty)
     {
         doUpdateMatrix(const_cast<Matrix44f*>(&mMatrix));
         mDirty = false;
-
         mDeviceDirty = true;
-        doUpdateDeviceMatrix(const_cast<Matrix44f*>(&mDeviceMatrix), mMatrix, mDevicePosture);
-        mDeviceDirty = false;
     }
 
-    else if (mDeviceDirty)
+    if (mDeviceDirty)
     {
         doUpdateDeviceMatrix(const_cast<Matrix44f*>(&mDeviceMatrix), mMatrix, mDevicePosture);
         mDeviceDirty = false;
     }
 }
 
+Matrix44f& Projection::getProjectionMatrixMutable()
+{
+    updateMatrixImpl_();
+    return mMatrix;
+}
+
 const Matrix44f& Projection::getDeviceProjectionMatrix() const
 {
     updateMatrixImpl_();
     return mDeviceMatrix;
+}
+
+void Projection::screenPosToCameraPos(Vector3f* camera_pos, const Vector3f& screen_pos) const
+{
+    doScreenPosToCameraPosTo(camera_pos, screen_pos);
+}
+
+void Projection::screenPosToCameraPos(Vector3f* camera_pos, const Vector2f& screen_pos) const
+{
+    screenPosToCameraPos(camera_pos, {screen_pos.x, screen_pos.y, 0.0f});
+}
+
+void Projection::project(Vector2f* dst, const Vector3f& camera_pos, const Viewport& viewport) const
+{
+    Vector3f screen_pos;
+    cameraPosToScreenPos(&screen_pos, camera_pos);
+    viewport.project(dst, screen_pos);
+}
+
+void Projection::unproject(Vector3f* world_pos, const Vector3f& screen_pos,
+                           const Camera& camera) const
+{
+    Vector3f camera_pos;
+    screenPosToCameraPos(&camera_pos, screen_pos);
+    camera.cameraPosToWorldPosByMatrix(world_pos, camera_pos);
+}
+
+void Projection::unprojectRay(Ray<Vector3f>* dst, const Vector3f& screen_pos,
+                              const Camera& camera) const
+{
+    Vector3f camera_pos;
+    screenPosToCameraPos(&camera_pos, screen_pos);
+    camera.unprojectRayByMatrix(dst, camera_pos);
 }
 
 }  // namespace sead
