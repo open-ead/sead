43f49e7150d5f406dd7f7c8e6642072cbca07220
William Donahue <baseballwilliam@gmail.com>
2026-09-04 11:59:23 -0400

    prim/BitFlag: assert bit index in makeMask (fixes #85)

--- include/prim/seadBitFlag.h
+++ include/prim/seadBitFlag.h
@@ -73,7 +73,11 @@ public:
     //       because it is present in debug info for pead in Super Mario Run.)
     T getMask(T v) const;
 
-    static T makeMask(int bit) { return T(1) << bit; }
+    static T makeMask(int bit)
+    {
+        SEAD_ASSERT(static_cast<u32>(bit) < sizeof(T) * 8);
+        return T(1) << bit;
+    }
 
     void setBit(int bit) { set(makeMask(bit)); }
     void resetBit(int bit) { reset(makeMask(bit)); }
